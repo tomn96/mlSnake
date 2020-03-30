9377fd083e2620e080b14a9a6d06cddaa7039dce
Tom Nissim <tomn96@gmail.com>
2020-03-31 02:13:20 +0300

    some testing

---
+++ src/game/TestGame.java
@@ -0,0 +1,78 @@
+package game;
+
+import java.awt.*;
+
+public class TestGame implements Runnable, Tickable, Renderable {
+    private Thread thread;
+    private boolean running = false;
+
+    Snake snake;
+
+    public TestGame() {
+        snake = new Snake();
+    }
+
+    @Override
+    public void run() {
+        double amountOfTicksPerSec = 1;
+        double ns = Math.pow(10, 9) / amountOfTicksPerSec;
+        double delta = 0;
+        int frames = 0;
+        long timer = System.currentTimeMillis();
+        long lastTime = System.nanoTime();
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1){
+                tick();
+                if (running) {
+                    render(null);
+                }
+                delta--;
+            }
+//            if (running) {
+//                render(null);
+//            }
+
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+//                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        running = true;
+        thread.start();
+    }
+
+    public void stop() {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        running = false;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        System.out.println(snake.getBoard());
+        System.out.println("\n");
+    }
+
+    @Override
+    public void tick() {
+        snake.move();
+    }
+
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.start();
+    }
+}
