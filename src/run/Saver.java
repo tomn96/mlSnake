d961ff9246e2055615cf25e64a14ca608145ad20
Tom Nissim <tomn96@gmail.com>
2020-04-02 01:08:30 +0300

    simple save methods

--- src/run/Saver.java
+++ src/run/Saver.java
@@ -1,18 +1,14 @@
 package run;
 
-import game.SmartSnake;
+import game.BaseSnake;
 
 import java.io.*;
 
 public class Saver {
 
-    public static void main(String[] args) {
-        SmartSnake snake = new SmartSnake();
-        while (!snake.isDead()) {
-            snake.tick();
-        }
+    public static void saveSnake(BaseSnake snake, String filepath) {
         try {
-            FileOutputStream fileOut = new FileOutputStream("/tmp/snakesaver/snake");
+            FileOutputStream fileOut = new FileOutputStream(filepath);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(snake);
             out.close();
@@ -20,17 +16,19 @@ public class Saver {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        SmartSnake s = null;
+    public static BaseSnake loadSnake(String filepath) {
+        BaseSnake snake = null;
         try {
-            FileInputStream fileIn = new FileInputStream("/tmp/snakesaver/snake");
+            FileInputStream fileIn = new FileInputStream(filepath);
             ObjectInputStream in = new ObjectInputStream(fileIn);
-            s = (SmartSnake) in.readObject();
+            snake = (BaseSnake) in.readObject();
             in.close();
             fileIn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        assert s != null;
+        return snake;
     }
 }
